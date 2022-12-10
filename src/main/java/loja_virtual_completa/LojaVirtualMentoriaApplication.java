822740d44d5f7917459673cd098930306653b0ca
root <root@smakambo-HP-Notebook>
2022-12-10 08:41:36 +0100

    Commit Inicial Projecto Mentoria Java

---
+++ src/main/java/loja_virtual_completa/LojaVirtualMentoriaApplication.java
@@ -0,0 +1,13 @@
+package loja_virtual_completa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualMentoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualMentoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/loja_virtual_completa/LojaVirtualMentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package loja_virtual_completa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualMentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
